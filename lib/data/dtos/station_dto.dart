dc00918632a5a8c2055f34877e7a1ed0e8301dd4
sonika-tang <sonikatang630@gmail.com>
2026-04-16 07:21:28 +0700

    FPM-30: Create Station DTO

---
+++ lib/data/dtos/station_dto.dart
@@ -0,0 +1,34 @@
+import 'package:bike_rental/models/station.dart';
+
+class StationDto {
+  static const String idKey = 'stationId';
+  static const String nameKey = 'name';
+  static const String latitudeKey = 'latitude';
+  static const String longitudeKey = 'longitude';
+  static const String bikesKey = 'bikes';
+
+  static Station fromJson(String id, Map<String, dynamic> json) {
+    assert(json[idKey] is String);
+    assert(json[nameKey] is String);
+    assert(json[latitudeKey] is double);
+    assert(json[longitudeKey] is double);
+
+    return Station(
+      stationId: id,
+      name: json[nameKey],
+      latitude: double.parse(json[latitudeKey].toString()),
+      longitude: double.parse(json[longitudeKey].toString()),
+      bikes: [],
+    );
+  }
+
+  Map<String, dynamic> toJson(Station station) {
+    return {
+      idKey: station.stationId,
+      nameKey: station.name,
+      latitudeKey: station.latitude.toString(),
+      longitudeKey: station.longitude.toString(),
+      bikesKey: station.bikes.map((bike) => bike.toJson()).toList(),
+    };
+  }
+}
