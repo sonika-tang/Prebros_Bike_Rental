9cf15bdc2b8de84f1634e85ecbc8bb6624538382
vesyean <veyseankem@gmail.com>
2026-04-17 22:50:48 +0700

    FPM-40 [update] ActivePassState

--- lib/ui/states/active_pass_state.dart
+++ lib/ui/states/active_pass_state.dart
@@ -1,5 +1,6 @@
 import 'package:bike_rental/models/pass.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class GlobalPassState extends ChangeNotifier {
   Pass? _activePass;
@@ -18,6 +19,18 @@ class GlobalPassState extends ChangeNotifier {
   void clearActivePass() {
     _activePass = null;
     notifyListeners();
+  }
+
+   String formatDate(DateTime? date) {
+    if (date == null) return "N/A";
+    return DateFormat("dd/MM/yyyy hh:mm a").format(date);
+  }
+
+  String get activePassDurationText {
+    if (activePass == null) return "No active pass";
+    final start = formatDate(activePass!.startDate);
+    final end = formatDate(activePass!.endDate);
+    return "Start: $start\nEnd: $end";
   }
 
   /// Toggle app theme between light and dark
