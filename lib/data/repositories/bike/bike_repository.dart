ab035567e3dd4652d7b3e647e3a277f89c73733d
SSONita <sarsovannita4@gmail.com>
2026-04-18 04:54:34 +0700

    FPM-50:[fix] Create BikeRepository Abstract

--- lib/data/repositories/bike/bike_repository.dart
+++ lib/data/repositories/bike/bike_repository.dart
@@ -1,6 +1,7 @@
 import 'package:bike_rental/models/bike.dart';
 
 abstract class BikeRepository {
+  Future<List<Bike>> fetchAllBikes();
   Future<List<Bike>> fetchBikesByStation(String stationId);
   Future<Bike?> fetchBikeById(String id);
 }
