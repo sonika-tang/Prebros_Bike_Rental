23562486c81ae978fd87dc186568967bd1bb0f1a
sonika-tang <sonikatang630@gmail.com>
2026-04-17 17:58:45 +0700

    FPM-33: [Fix] Firebase StationRepository to get the list of bike for each station from firebase

--- lib/data/repositories/station/station_repository_firebase.dart
+++ lib/data/repositories/station/station_repository_firebase.dart
@@ -1,25 +1,44 @@
 import 'dart:convert';
+import 'package:bike_rental/data/dtos/bike_dto.dart';
 import 'package:bike_rental/data/dtos/station_dto.dart';
 import 'package:bike_rental/data/repositories/station/station_repository.dart';
+import 'package:bike_rental/models/bike.dart';
 import 'package:bike_rental/models/station.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 
 class StationRepositoryFirebase implements StationRepository {
-  static final Uri baseUri = Uri.https(dotenv.env['FIREBASE_DB_URL'] ?? '');
-  static final Uri stationUrl = baseUri.replace(path: '/stations.json');
+  static final String baseUrl = dotenv.env['FIREBASE_DB_URL'] ?? '';
+  static Uri get stationUrl => Uri.parse('https://$baseUrl/stations.json');
+  static Uri get bikesUrl => Uri.parse('https://$baseUrl/bikes.json');
 
   @override
   Future<List<Station>> getAllStations() async {
     try {
       final response = await http.get(stationUrl);
+      final bikesResponse = await http.get(bikesUrl);
+
       if (response.statusCode == 200) {
         final Map<String, dynamic> data = json.decode(response.body);
-        final List<Station> stations = [];
-        data.forEach((key, value) {
-          stations.add(StationDto.fromJson(key, value));
-        });
-        return stations;
+        
+        List<Bike> allBikes = [];
+        if (bikesResponse.statusCode == 200 && bikesResponse.body != 'null') {
+          final bikeData = json.decode(bikesResponse.body);
+          if (bikeData is Map) {
+            allBikes = bikeData.entries
+                .map((e) => BikeDto.fromJson(e.key, e.value))
+                .toList();
+          } else if (bikeData is Iterable) {
+            for (var b in bikeData) {
+              if (b != null) allBikes.add(BikeDto.fromJson('', b));
+            }
+          }
+        }
+
+        return data.entries.map((e) {
+          final stationBikes = allBikes.where((b) => b.stationId == e.key).toList();
+          return StationDto.fromJson(e.key, e.value, bikes: stationBikes);
+        }).toList();
       } else {
         throw Exception('Failed to load stations: ${response.statusCode}');
       }
@@ -30,11 +49,29 @@ class StationRepositoryFirebase implements StationRepository {
 
   @override
   Future<Station?> getStationById(String id) async {
-    final specificStationUrl = baseUri.replace(path: '/stations/$id.json');
-    final response = await http.get(specificStationUrl);
+    final url = Uri.parse('https://$baseUrl/stations/$id.json');
+    final response = await http.get(url);
     if (response.statusCode == 200 && response.body != 'null') {
       final data = json.decode(response.body);
-      return StationDto.fromJson(id, data);
+
+      final bikesResponse = await http.get(bikesUrl);
+      List<Bike> allBikes = [];
+      if (bikesResponse.statusCode == 200 && bikesResponse.body != 'null') {
+        final bikeData = json.decode(bikesResponse.body);
+        if (bikeData is Map) {
+          allBikes = bikeData.entries
+              .map((e) => BikeDto.fromJson(e.key, e.value))
+              .toList();
+        } else if (bikeData is Iterable) {
+          for (var b in bikeData) {
+            if (b != null) allBikes.add(BikeDto.fromJson('', b));
+          }
+        }
+      }
+      
+      final stationBikes = allBikes.where((b) => b.stationId == id).toList();
+
+      return StationDto.fromJson(id, data, bikes: stationBikes);
     }
     return null;
   }
