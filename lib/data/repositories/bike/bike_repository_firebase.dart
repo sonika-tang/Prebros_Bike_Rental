b6dca8b5460df4ec6ad47f53db611ed648e95501
SSONita <sarsovannita4@gmail.com>
2026-04-18 07:59:49 +0700

    FPM-48 [fix] Implement Firebase BikeRepository

--- lib/data/repositories/bike/bike_repository_firebase.dart
+++ lib/data/repositories/bike/bike_repository_firebase.dart
@@ -6,23 +6,18 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 
 class BikeRepositoryFirebase implements BikeRepository {
-  static final Uri baseUri = Uri.https(dotenv.env['FIREBASE_DB_URL'] ?? '');
-  static final Uri bikeUrl = baseUri.replace(path: '/bikes.json');
+  static final Uri baseUri = Uri.https(dotenv.env['FIREBASE_DB_URL'] ?? '', '');
+  static final Uri bikesUrl = baseUri.replace(path: '/bikes.json');
 
   @override
   Future<List<Bike>> fetchAllBikes() async {
     try {
-      final response = await http.get(bikeUrl);
+      final response = await http.get(bikesUrl);
       if (response.statusCode == 200 && response.body != 'null') {
         final data = json.decode(response.body);
-        if (data is Map) {
+        if (data is Map<String, dynamic>) {
           return data.entries
               .map((e) => BikeDto.fromJson(e.key, e.value))
-              .toList();
-        } else if (data is Iterable) {
-          return data
-              .where((b) => b != null)
-              .map((b) => BikeDto.fromJson('', b))
               .toList();
         }
       }
@@ -35,7 +30,7 @@ class BikeRepositoryFirebase implements BikeRepository {
   @override
   Future<Bike?> fetchBikeById(String id) async {
     try {
-      final url = Uri.parse('https://$baseUri/bikes/$id.json');
+      final url = baseUri.replace(path: '/bikes/$id.json');
       final response = await http.get(url);
       if (response.statusCode == 200 && response.body != 'null') {
         final data = json.decode(response.body);
@@ -50,20 +45,15 @@ class BikeRepositoryFirebase implements BikeRepository {
   @override
   Future<List<Bike>> fetchBikesByStation(String stationId) async {
     try {
-      final url = Uri.parse('https://$baseUri/bikes.json').replace(
+      final url = bikesUrl.replace(
         queryParameters: {'orderBy': '"stationId"', 'equalTo': '"$stationId"'},
       );
       final response = await http.get(url);
       if (response.statusCode == 200 && response.body != 'null') {
         final data = json.decode(response.body);
-        if (data is Map) {
+        if (data is Map<String, dynamic>) {
           return data.entries
               .map((e) => BikeDto.fromJson(e.key, e.value))
-              .toList();
-        } else if (data is Iterable) {
-          return data
-              .where((b) => b != null)
-              .map((b) => BikeDto.fromJson('', b))
               .toList();
         }
       }
