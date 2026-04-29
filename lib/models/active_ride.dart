a564c746ae849c074d26e4821e151c99e48b4913
sonika-tang <sonikatang630@gmail.com>
2026-04-29 20:47:13 +0700

    Update model to use active ride to map the bike and station

---
+++ lib/models/active_ride.dart
@@ -0,0 +1,11 @@
+class ActiveRide {
+  final String bikeId;
+  final String stationName;
+  final String slot;
+
+  const ActiveRide({
+    required this.bikeId,
+    required this.stationName,
+    required this.slot,
+  });
+}
